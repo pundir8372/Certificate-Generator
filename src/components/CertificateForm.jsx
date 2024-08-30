3af4de0cd7411652eba73f6f43f257745c5a2035
pundir8372 <149288710+pundir8372@users.noreply.github.com>
2024-08-30 22:03:32 +0530

    Update CertificateForm.jsx

--- src/components/CertificateForm.jsx
+++ src/components/CertificateForm.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { db, ref, get, child } from '../firebase'; // Ensure this path is correct
+import { db, ref, get, child } from '../Firebase'; // Ensure this path is correct
 import { useNavigate } from 'react-router-dom';
 import './CertificateForm.css';
 
